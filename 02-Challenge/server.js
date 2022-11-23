45bcfce0c85cc44ae06f168778de2b921bc7b930
DaleDuiv <110407486+DaleDuiv@users.noreply.github.com>
2022-11-24 00:24:20 +1030

    adjusting to find bug

--- 02-Challenge/server.js
+++ 02-Challenge/server.js
@@ -15,21 +15,9 @@ app.use(express.json());
 
 app.use(express.static("./Develop/public"));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
-);
-
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
-);
-
-app.get("*", (req, res) => 
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
-)
-
 
 app.get("/api/notes", function(req, res) {
-  readFileAsync("./develop/db/db.json", "utf-8").then(function(data) {
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
     notes = [].concat(JSON.parse(data))
     res.json(notes);
   })
@@ -38,11 +26,11 @@ app.get("/api/notes", function(req, res) {
 
 app.post("/api/notes", function(req, res) {
   const note = req.body;
-  readFileAsync("./develop/db/db.json", "utf-8").then(function(data) {
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
   const notes = [].concat(JSON.parse(data));
   notes.id = notes.length + 1
   notes.push(note);
-  return notes;  
+  return notes  
   }).then(function(notes) {
     writeFileAsync("./develop/db/db.json", JSON.stringify(notes));
     res.json(note);
@@ -51,10 +39,10 @@ app.post("/api/notes", function(req, res) {
 
 app.delete("/api/notes/:id", function(req,res) {
   const idToDelete = parseInt(req.params.id);
-  readFileAsync("./develop/db/db.json", "utf-8").then(function(data) {
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
     const notes = [].concat(JSON.parse(data));
     const newNotesData = []
-    for (let i = 0; i < notes.length; i++) {
+    for (let i = 0; i<notes.length; i++) {
       if(idToDelete !== notes[i].id) {
         newNotesData.push(notes[i])
       }
@@ -66,7 +54,22 @@ app.delete("/api/notes/:id", function(req,res) {
   })
 });
 
-app.listen(PORT, () => {
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
+);
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
+);
+
+
+app.get("*", (req, res) => 
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
+)
+
+
+
+app.listen(PORT, function() {
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 
 })
